9eacc42497c92fddd11b7fbe6d8e9564dc844e3f
Josh Patterson <Josh@NTDLS.com>
2024-08-29 17:24:57 -0400

    Added field/row helpers.

--- Payloads/KbQueryResult.cs
+++ Payloads/KbQueryResult.cs
@@ -13,6 +13,47 @@
             Fields.Add(new KbQueryField(name));
         }
 
+        /// <summary>
+        /// Returns the integer index of the field name, throws exception if not found.
+        /// </summary>
+        public int IndexOf(string fieldName)
+        {
+            int index = 0;
+            foreach (var field in Fields)
+            {
+                if (field.Name.Equals(fieldName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return index;
+                }
+                index++;
+            }
+
+            throw new Exception($"Field {fieldName} was not found in the collection.");
+        }
+
+        /// <summary>
+        /// Returns the value of the given field on the given row.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public string? RowValue(KbQueryRow row, string fieldName)
+        {
+            int fieldIndex = IndexOf(fieldName);
+            return row.Values[fieldIndex];
+        }
+
+        /// <summary>
+        /// Returns the value of the given field index on the given row.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public string? RowValue(KbQueryRow row, int fieldIndex)
+        {
+            return row.Values[fieldIndex];
+        }
+
         public void AddRow(List<string?> values)
         {
             Rows.Add(new KbQueryRow(values));
